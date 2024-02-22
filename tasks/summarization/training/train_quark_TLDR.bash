dc7569ad77605f34c22cfec571ed0bc502aa0b2b
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-02-22 15:56:57 +0100

    quick save

--- tasks/summarization/training/train_quark_TLDR.bash
+++ tasks/summarization/training/train_quark_TLDR.bash
@@ -2,8 +2,7 @@
 
 #SBATCH -n 1
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus=a100_80gb:1
-#SBATCH --gres=gpumem:80g
+#SBATCH --gpus=a100-pcie-40gb:4
 #SBATCH --mem-per-cpu=48000
 #SBATCH --time=24:00:00
 #SBATCH --output="output/quark_train_TLDR_5q_sampling_stage_1.out"
