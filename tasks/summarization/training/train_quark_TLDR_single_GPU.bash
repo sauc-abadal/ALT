0571f5b36b22ff00649f9d4c2779c03acd0e95b5
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-02-23 15:00:16 +0100

    quick save

--- tasks/summarization/training/train_quark_TLDR_single_GPU.bash
+++ tasks/summarization/training/train_quark_TLDR_single_GPU.bash
@@ -8,4 +8,6 @@
 #SBATCH --output="output/quark_train_TLDR_5q_single_GPU_sampling_stage_1.out"
 #SBATCH --open-mode=append
 
+python set_wandb_run_id.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml
+
 accelerate launch --config_file tasks/summarization/training/default_config_single_GPU.yaml tasks/summarization/training/quark_train_single_GPU.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml
