55561c743bd67f6de36b4c2f5f261789f2fe6303
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-02-24 10:36:42 +0100

    quick save

--- tasks/summarization/training/sample_and_reward_quark_TLDR.bash
+++ tasks/summarization/training/sample_and_reward_quark_TLDR.bash
@@ -6,9 +6,11 @@
 #SBATCH --gres=gpumem:80g
 #SBATCH --mem-per-cpu=48000
 #SBATCH --time=24:00:00
-#SBATCH --output="output/quark_sampling_TLDR_5q_sampling_stage_2.out"
+#SBATCH --output="output/quark_sampling_and_reward_TLDR_5q_sampling_stage_2.out"
 #SBATCH --open-mode=append
 
 python set_wandb_run_id.py --config tasks/summarization/training/train_quark_TLDR_config.yml
 
 python tasks/summarization/training/quark_sampling.py --config tasks/summarization/training/train_quark_TLDR_config.yml --first_iter False --split train
+
+python tasks/summarization/training/quark_reward.py --config tasks/summarization/training/train_quark_TLDR_config.yml --first_iter False --split train
