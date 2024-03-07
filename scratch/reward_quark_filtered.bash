495fd185af541189ca09d4883d27ea6e8e94c450
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-03-07 18:36:36 +0100

    quick save

---
+++ scratch/reward_quark_filtered.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=a100_80gb:1
+#SBATCH --gres=gpumem:80g
+#SBATCH --mem-per-cpu=96000
+#SBATCH --time=2:00:00
+#SBATCH --output="output/quark_reward_TLDR_5q_sampling_stage_2_filtered.out"
+#SBATCH --open-mode=append
+
+python scratch/quark_reward_filtered.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml --root_path /cluster/work/sachan/sauc/nlf/quark_TLDR_5q_v2/sampling --input_json_file quark_sampling_data_train_stage_2_filtered.json  
