74e35db9a1e4185b9fe2fdef83872963971a6fca
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-03-19 09:33:36 +0100

    quick save

--- tasks/summarization/training/bash_scripts/full_eval_quark_TLDR.bash
+++ tasks/summarization/training/bash_scripts/full_eval_quark_TLDR.bash
@@ -6,9 +6,9 @@
 #SBATCH --gres=gpumem:80g
 #SBATCH --mem-per-cpu=96000
 #SBATCH --time=2:00:00
-#SBATCH --output="output/quark_v2_full_eval_TLDR_5q_sampling_stage_2.out"
+#SBATCH --output="output/quark_full_eval_TLDR_5q_sampling_stage_1.out"
 #SBATCH --open-mode=append
 
-python tasks/summarization/training/quark_sampling.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml --first_iter False --split valid
-python tasks/summarization/training/quark_reward.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml --split valid
-python tasks/summarization/training/quark_eval.py --config tasks/summarization/training/train_quark_TLDR_single_GPU_config.yml
+accelerate launch --config_file /cluster/project/sachan/sauc/nlf/tasks/summarization/training/configs/accelerate_config_1gpu.yaml tasks/summarization/training/quark_sampling.py --config tasks/summarization/training/configs/quark_TLDR_config.yml --first_iter False --split valid
+mv /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/quark_sampling_data_valid_stage_1_worker_0.json /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/quark_sampling_data_valid_stage_1.json
+python tasks/summarization/training/quark_eval.py --config tasks/summarization/training/quark_TLDR_config.yml
