609b5561299bdc3e336fc3384131e0500a00c3bb
Sauec AbadalLloret <sabadal@eu-login-45.euler.ethz.ch>
2024-03-21 15:13:26 +0100

    updated bash scripts

--- tasks/summarization/training/bash_scripts/full_eval_quark_TLDR.bash
+++ tasks/summarization/training/bash_scripts/full_eval_quark_TLDR.bash
@@ -6,9 +6,10 @@
 #SBATCH --gres=gpumem:80g
 #SBATCH --mem-per-cpu=96000
 #SBATCH --time=2:00:00
-#SBATCH --output="output/quark_full_eval_TLDR_5q_sampling_stage_1.out"
+#SBATCH --output="output/quark_full_eval_TLDR_5q_sampling_stage_2.out"
 #SBATCH --open-mode=append
 
-mv /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_valid_stage_1_worker_0.json /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_valid_stage_1.json
+accelerate launch --config_file /cluster/project/sachan/sauc/nlf/tasks/summarization/training/configs/accelerate_config_1gpu.yaml tasks/summarization/training/quark_sampling.py --config tasks/summarization/training/configs/quark_TLDR_config.yaml --first_iter False --split valid
+mv /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_valid_stage_2_worker_0.json /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_valid_stage_2.json
 python tasks/summarization/training/quark_reward.py --config tasks/summarization/training/configs/quark_TLDR_config.yaml --split valid
 python tasks/summarization/training/quark_eval.py --config tasks/summarization/training/configs/quark_TLDR_config.yaml
