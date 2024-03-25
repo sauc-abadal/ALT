a0b7068b1646a78c82b99af9e66a5d45c2fcd264
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-03-25 16:30:20 +0100

    fixed typo

--- tasks/summarization/training/bash_scripts/test_reward_quark_TLDR_train_split_2gpu.bash
+++ tasks/summarization/training/bash_scripts/test_reward_quark_TLDR_train_split_2gpu.bash
@@ -3,7 +3,7 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=rtx_3090:2
-#SBATCH --gres=gpumem:80g
+#SBATCH --gres=gpumem:24g
 #SBATCH --mem-per-cpu=96000
 #SBATCH --time=2:00:00
 #SBATCH --output="output/quark_test_rewarding_TLDR_5q_train_split_sampling_stage_2.out"
