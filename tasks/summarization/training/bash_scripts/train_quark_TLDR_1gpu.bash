b4dc537006c967a22c190458f7d2aeef966d7ca9
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-04-05 14:12:07 +0200

    quick save

--- tasks/summarization/training/bash_scripts/train_quark_TLDR_1gpu.bash
+++ tasks/summarization/training/bash_scripts/train_quark_TLDR_1gpu.bash
@@ -8,4 +8,4 @@
 #SBATCH --output="output/quark_training_TLDR_5q_v6_noKL_1gpu_iter_1.out"
 #SBATCH --open-mode=append
 
-accelerate launch --config_file /cluster/project/sachan/sauc/nlf/tasks/summarization/training/configs/accelerate_config_ds_1gpu_ds_opt_ds_sch_cpu_off.yaml tasks/summarization/training/quark_train.py --config tasks/summarization/training/configs/quark_TLDR_config.yaml --iteration 1 --input_sampling_file /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_train_split_iter_1.json --ds_optimizer --ds_scheduler
+accelerate launch --config_file /cluster/project/sachan/sauc/nlf/tasks/summarization/training/configs/accelerate_config_ds_1gpu_ds_opt_ds_sch_cpu_off.yaml tasks/summarization/training/quark_train_noKL.py --config tasks/summarization/training/configs/quark_TLDR_config.yaml --iteration 1 --input_sampling_file /cluster/work/sachan/sauc/nlf/quark_TLDR_5q/sampling/quark_sampling_data_train_split_iter_1.json --ds_optimizer --ds_scheduler
