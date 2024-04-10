dfc2290942718bcc2f18d1b6b4ad15fc9656387c
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-04-10 16:21:27 +0200

    quick sync

--- tasks/summarization/training/bash_scripts/vllm_sampling_start_run.sh
+++ tasks/summarization/training/bash_scripts/vllm_sampling_start_run.sh
@@ -8,4 +8,4 @@
 
 source /cluster/project/sachan/sauc/anaconda3/bin/activate sample
 
-python tasks/summarization/training/bash_scripts/vllm_sampling.py --input_file $1 --output_dir $2 --split_number $3 --total_splits $4 --model_path $5 --tokenizer_path $6 --data_split $7 --num_generations $8 --temperature $9 --top_p $10 --max_new_tokens $11
+python tasks/summarization/training/vllm_sampling.py --input_file $1 --output_dir $2 --split_number $3 --total_splits $4 --model_path $5 --tokenizer_path $6 --data_split $7 --num_generations $8 --temperature $9 --top_p $10 --max_new_tokens $11
