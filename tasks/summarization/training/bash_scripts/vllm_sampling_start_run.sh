1d1dca42cc38a6906d10f495be45724d8d381d02
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-04-10 21:23:02 +0200

    quick save

--- tasks/summarization/training/bash_scripts/vllm_sampling_start_run.sh
+++ tasks/summarization/training/bash_scripts/vllm_sampling_start_run.sh
@@ -8,4 +8,7 @@
 
 source /cluster/project/sachan/sauc/anaconda3/bin/activate sample
 
-python tasks/summarization/training/vllm_sampling.py --input_file "$1" --output_dir "$2" --split_number "$3" --total_splits "$4" --model_path "$5" --tokenizer_path "$6" --data_split "$7" --num_generations "$8" --temperature "$9" --top_p "$10" --max_new_tokens "$11"
+# python tasks/summarization/training/vllm_sampling.py --input_file "$1" --output_dir "$2" --split_number "$3" --total_splits "$4" --model_path "$5" --tokenizer_path "$6" --data_split "$7" --num_generations "$8" --temperature "$9" --top_p "$10" --max_new_tokens "$11"
+
+python tasks/summarization/training/vllm_sampling.py --input_file "/cluster/work/sachan/NLF/sampled_prompts_iter_3.json" --output_dir "/cluster/work/sachan/NLF/output_iter_3" --split_number 0 --total_splits 8 --model_path "/cluster/work/sachan/NLF/model/iter_2/model_ckp_5120" --tokenizer_path "/cluster/work/sachan/NLF/quark_TLDR_5q_tokenizer" --data_split "train" --num_generations 96 --temperature 0.9 --top_p 0.9 --max_new_tokens 64
+
