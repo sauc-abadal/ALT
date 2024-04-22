a2d75ef559cb5f8bf4c0a1990dab21d692db43a8
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-04-22 16:26:57 +0200

    quick save

--- tasks/summarization/training/bash_scripts/PPO_feedback_start_run.sh
+++ tasks/summarization/training/bash_scripts/PPO_feedback_start_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=3:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem-per-cpu=96G
 #SBATCH --output="/cluster/work/sachan/NLF/quark/slurm_output/ppo_gpt3.5_feedback_%j.out"
 #SBATCH --open-mode=append
