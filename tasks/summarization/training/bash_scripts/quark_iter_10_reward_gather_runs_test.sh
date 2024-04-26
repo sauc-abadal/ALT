1abf4aadbcb71f83ea85dfc279383c8888edfa7f
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-04-26 10:54:19 +0200

    quick save

--- tasks/summarization/training/bash_scripts/quark_iter_10_reward_gather_runs_test.sh
+++ tasks/summarization/training/bash_scripts/quark_iter_10_reward_gather_runs_test.sh
@@ -11,15 +11,11 @@ num_generations=1
 # concatenate previously sampled jsonl files (8 threads) into a single jsonl file
 bash tasks/summarization/training/bash_scripts/concatenate_jsonl.sh \
     "$input_sampling_file" \
-    "${output_dir}/test_output_"{0..7}.json
+    "${output_dir}/test_output_"{0..3}.json
 
 # Submit SLURM jobs and capture job IDs
-reward1=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 0 8 "$num_generations" | awk '{print $4}')
-reward2=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 1 8 "$num_generations" | awk '{print $4}')
-reward3=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 2 8 "$num_generations" | awk '{print $4}')
-reward4=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 3 8 "$num_generations" | awk '{print $4}')
-reward5=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 4 8 "$num_generations" | awk '{print $4}')
-reward6=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 5 8 "$num_generations" | awk '{print $4}')
-reward7=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 6 8 "$num_generations" | awk '{print $4}')
-reward8=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 7 8 "$num_generations" | awk '{print $4}')
+reward1=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 0 4 "$num_generations" | awk '{print $4}')
+reward2=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 1 4 "$num_generations" | awk '{print $4}')
+reward3=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 2 4 "$num_generations" | awk '{print $4}')
+reward4=$(sbatch tasks/summarization/training/bash_scripts/quark_reward_start_run.sh "$config" "$input_sampling_file" "$output_dir" 3 4 "$num_generations" | awk '{print $4}')
 
