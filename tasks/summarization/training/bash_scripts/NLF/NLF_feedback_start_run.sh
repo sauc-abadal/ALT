a4dac199d2dd9b9211a37fbd5fc0acf28db65c5b
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-05-08 11:07:26 +0200

    quick save

--- tasks/summarization/training/bash_scripts/NLF/NLF_feedback_start_run.sh
+++ tasks/summarization/training/bash_scripts/NLF/NLF_feedback_start_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=6:00:00
-#SBATCH --mem-per-cpu=96G
-#SBATCH --output="/cluster/work/sachan/NLF/nlf_v2/slurm_output/gpt3.5_feedback_%j.out"
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=120G
+#SBATCH --output="/cluster/work/sachan/NLF/nlf_v3/slurm_output/gpt3.5_feedback_%j.out"
 #SBATCH --open-mode=append
 
 source /cluster/project/sachan/sauc/anaconda3/bin/activate sample
