231fc1fa40febc615b048e351688a846b848169e
Sauc Abadal Lloret <s.abadal.lloret@gmail.com>
2024-05-28 09:37:10 +0200

    quick save

--- tasks/hh/training/bash_scripts/baselines/SFT_eval_feedback.sh
+++ tasks/hh/training/bash_scripts/baselines/SFT_eval_feedback.sh
@@ -14,8 +14,8 @@ file_prefix_valid="SFT_sampling_data_test_1000subset"
 
 # 1. ---------------- EVAL FEEDBACK (valid) ----------------
 # Submit SLURM FEEDBACK jobs (dependency on 'sample_v') and capture job IDs
-feedback_v1=$(sbatch --dependency=afterok:$sample_v tasks/hh/training/bash_scripts/NLF/NLF_eval_feedback_start_run.sh "$config" "$input_sampling_file_valid" "$output_dir" 0 2 $num_generations_valid | awk '{print $4}')
-feedback_v2=$(sbatch --dependency=afterok:$sample_v tasks/hh/training/bash_scripts/NLF/NLF_eval_feedback_start_run.sh "$config" "$input_sampling_file_valid" "$output_dir" 1 2 $num_generations_valid | awk '{print $4}')
+feedback_v1=$(sbatch tasks/hh/training/bash_scripts/NLF/NLF_eval_feedback_start_run.sh "$config" "$input_sampling_file_valid" "$output_dir" 0 2 $num_generations_valid | awk '{print $4}')
+feedback_v2=$(sbatch tasks/hh/training/bash_scripts/NLF/NLF_eval_feedback_start_run.sh "$config" "$input_sampling_file_valid" "$output_dir" 1 2 $num_generations_valid | awk '{print $4}')
 
 # concatenate 2 feedback files (dependency on 'feedback_v0..1') and capture job ID
 feedback_v=$(sbatch --dependency=afterok:$feedback_v1:$feedback_v2 tasks/hh/training/bash_scripts/sbatch_concatenate_jsonl.sh \
